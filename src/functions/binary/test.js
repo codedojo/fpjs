ae4fc084236ec8fcac1f3d995ae35c7992360e67
Oleg Polyakov <olegpolyako@outlook.com>
2017-10-15 13:50:34 +0300

    Add functions/binary tests

---
+++ src/functions/binary/test.js
@@ -0,0 +1,23 @@
+const binary = require('./solution');
+
+describe('binary', () => {
+    test('возвращает функцию', () => {
+        expect(binary(() => {})).toBeInstanceOf(Function);
+    });
+
+    test('возращаемая функция принимает два аргумента', () => {
+        let fn = binary(() => {});
+
+        expect(fn.length).toBe(2);
+    });
+
+    test('возращаемая функция вызывает переданную функцию с двумя агрументами', () => {
+        let mock = jest.fn();
+        let fn = binary(mock);
+
+        fn(3, 4);
+
+        expect(mock.mock.calls[0][0]).toBe(3);
+        expect(mock.mock.calls[0][1]).toBe(4);
+    });
+});
